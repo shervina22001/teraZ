852ef04428fecce4d9e4b397377490aad8fd98f5
nabilarahmanisaputriarzetta <nabila22030@mail.unpad.ac.id>
2025-10-01 14:06:16 +0700

    frontend-login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/', function () {
+    return Inertia::render('LandingPage');
+});
+
 Route::get('/login', function () {
     return Inertia::render('LoginPage');
 });
